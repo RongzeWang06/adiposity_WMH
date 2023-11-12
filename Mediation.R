3588f1e273a61ffebcdc10805bf082e65eb86eb1
RongzeWang06 <145853695+RongzeWang06@users.noreply.github.com>
2023-11-12 22:32:10 +0800

    Add files via upload

--- Mediation.R
+++ Mediation.R
@@ -1,15 +1,15 @@
 library(mediation)
 scaled_BP<-lapply(WMH_BP, scale)
-b <- lm(SBP~BMI+Sex+Age, data=scaled_BP)
-c <- lm(WMH_log~SBP+BMI+Sex+Age, data=scaled_BP)
+b <- lm(SBP~BMI+Sex+Age+Site2, data=scaled_BP)
+c <- lm(WMH_log~SBP+BMI+Sex+Age+Site2, data=scaled_BP)
 contcont <- mediate(b, c, sims=50, treat="BMI", mediator="SBP", robustSE = TRUE)
 summary(contcont)
 contcont
 
-PathA<-lm(SBP~BMI+Sex+Age, data=scaled_BP)
+PathA<-lm(SBP~BMI+Sex+Age+Site2, data=scaled_BP)
 summary(PathA)
 confint(PathA)
 
-PathB<-lm(WMH_log~SBP+Sex+Age, data=scaled_BP)
+PathB<-lm(WMH_log~SBP+Sex+Age+Site2, data=scaled_BP)
 summary(PathB)
 confint(PathB)
